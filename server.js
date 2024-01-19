34d4fdbd22c5751331d5fa4756adca9848ddae29
catherinep-pnnl <catherine.pham@pnnl.gov>
2024-01-19 10:43:53 -0800

    WIP post

--- server.js
+++ server.js
@@ -12,39 +12,51 @@ const sequelize = new Sequelize("dinosaurs", "root", process.env.DB_PASSWORD, {
 
 // Define a model
 const Dinosaur = sequelize.define("Dinosaur", {
-  name: { type: Sequelize.STRING, allowNull: false },
-  votes: { type: Sequelize.INTEGER, allowNull: false },
-  bracket: { type: Sequelize.STRING, allowNull: false },
+  name: { type: Sequelize.STRING },
+  person: { type: Sequelize.STRING },
+  votes: { type: Sequelize.INTEGER },
+  bracket: { type: Sequelize.STRING },
 });
 
 // Sync the model with the database
-sequelize.sync({ force: true }); // TODO: remove true when ready to go to prod
+sequelize.sync(); // add force: true to drop the db and recreate it
 
 // Middleware
 app.use(express.json());
 
 // Endpoints
-app.get("/dinosaurs", async (req, res) => {
+app.get("/api/dinosaurs", async (req, res) => {
   const dinosaurs = await Dinosaur.findAll();
   res.json(dinosaurs);
 });
 
-app.post("/dinosaurs", async (req, res) => {
-  const { name, votes } = req.body;
-  const dinosaur = await Dinosaur.update({ name, votes }); // have to find by name and bracket, and update the votes
+app.post("/api/dinosaurs", async (req, res) => {
+  const dinosaur = await Dinosaur.create(req.body);
   res.json(dinosaur);
 });
 
-// Protection middleware
-app.use((req, res, next) => {
-  const token = req.headers.authorization;
-  if (token !== "secret-token") {
-    return res.status(401).json({ message: "Unauthorized" });
+app.post("/api/dinosaurs/:bracket/:name", async (req, res) => {
+  const dinosaur = await Dinosaur.findOne({
+    where: { bracket: req.params.bracket, name: req.params.bracket },
+  });
+  if (dinosaur) {
+    dinosaur.increment("votes", { by: 1 });
+    res.json(dinosaur);
+  } else {
+    console.log("error", dinosaur);
   }
-  next();
 });
 
+// Protection middleware
+// app.use((req, res, next) => {
+//   const token = req.headers.authorization;
+//   if (token !== "secret-token") {
+//     return res.status(401).json({ message: "Unauthorized" });
+//   }
+//   next();
+// });
+
 // Start the server
-app.listen(3000, () => {
+app.listen(3001, () => {
   console.log("Server started on port 3001");
 });
